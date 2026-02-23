59745e6dfb3c617e203c356236439c3eb240592a
Duvan Vanegas <152895830+Duvanegas0608@users.noreply.github.com>
2026-02-23 10:59:37 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Alvatek_Solutions/" >
       //<HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
