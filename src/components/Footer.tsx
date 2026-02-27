30af52e11b9be5e0443c58c77820df6e775fbc14
Duvan Vanegas <152895830+Duvanegas0608@users.noreply.github.com>
2026-02-27 18:44:29 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,19 +10,17 @@ const Footer = () => {
           {/* Company Info */}
           <div className="space-y-4">
             <div className="flex items-center space-x-2">
-              <div className="w-40 h-40 rounded-lg flex items-center justify-center overflow-hidden" >
+              <div className="w-36 h-36 rounded-lg flex items-center justify-center overflow-hidden" >
                 <img src={logo} alt="Alvatek Solutions" classnName="w-full h-full object-contain" />
               </div>
               {/*<div className="flex flex-col">
                 <span className="font-bold text-secondary-foreground text-lg leading-tight">Alvatek</span>
                 <span className="text-xs text-secondary-foreground/70 leading-tight">Solutions S.A.S.</span>
               </div>*/}
-              <div>
-                <p className="text-sm text-secondary-foreground/80">
-                  Soluciones profesionales en automatización industrial y control eléctrico.
-                </p>
-              </div>
             </div>
+            <p className="text-sm text-secondary-foreground/80">
+                Soluciones profesionales en automatización industrial y control eléctrico.
+              </p>
           </div>
 
           {/* Quick Links */}
